8f83b775e73f4be19505f707c88c48acb1df349a
andreahuerfanob <andreahuerfano@live.com>
2020-01-22 12:08:16 -0500

    poly

--- Client/src/components/Webmap.js
+++ Client/src/components/Webmap.js
@@ -6,53 +6,45 @@ import { EditControl } from "react-leaflet-draw";
 export default function Webmap() {
   const prov = OpenStreetMapProvider();
   const GeoSearchControlElement = withLeaflet(SearchControl);
-  
 
   return (
     <Map center={[18, 0]} zoom={2.5}>
       >
       <TileLayer
-        url="https://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}"
-        attribution='Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'.{format}"
+        attribution="Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
         time=""
         tilematrixset="GoogleMapsCompatible_Level"
         minZoom="1.5"
-        maxZoom="8"
+        maxZoom="19"
         format="jpg"
       />
       <GeoSearchControlElement
         provider={prov}
         showMarker={true}
-        showPopup={false}
+        showPopup={true}
         popupFormat={({ result }) => result.label}
         maxMarkers={3}
         retainZoomLevel={false}
         animateZoom={true}
-        autoClose={false}
+        autoClose={true}
         searchLabel={"Enter address, please"}
         keepResult={true}
 
         // position = "center"
-        />
+      />
       <FeatureGroup>
         <EditControl
-          position="topright"
-          
-          onEdited={(event) => this.onEditPath(event)}
-          onCreated={(event) => this.onCreateHandler(event)}
-          onDeleted={(event) =>this.onDeleted(event)}
           draw={{
-          polygon: true,
-          circle: false,
-          marker: false,
-          polyline: false,
-          rectangle: false,
-          circlemarker: false,
+            polygon: true,
+            circle: false,
+            marker: false,
+            polyline: false,
+            rectangle: false,
+            circlemarker: false
           }}
         />
       </FeatureGroup>
-
-
     </Map>
   );
 }
